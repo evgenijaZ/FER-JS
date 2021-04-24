6d0bb9e8fac8b3b3b7be179f7d1962dfaa74e22b
Yevheniia Zubrych <evg.zubrich@gmail.com>
2021-04-24 13:54:31 +0300

    Face detection on FE

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const resNetPredictor = require('../src/predictor')
 const faceDetector = require('../src/face_detector')
+const path = require("path");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    faceDetector.detect("./src/data/images/img.png")
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+router.post('/', function (req, res) {
+    const prepare_faces = async () => {
+        let coordinates = []
+        coordinates.push(await faceDetector.detect("./src/data/images/img.png"));
+        coordinates.push(await faceDetector.detect("./src/data/images/img_1.png"));
+        coordinates.push(await faceDetector.detect("./src/data/images/img_2.png"));
+        return coordinates;
+    }
+    prepare_faces().then(coordinates => console.log("index.js:" + coordinates + "."))
+
     const IMG1 = './src/data/images/S010_006_00000013.png'
     const IMG2 = './src/data/images/S010_006_00000014.png'
     const IMG3 = './src/data/images/S010_006_00000015.png'
