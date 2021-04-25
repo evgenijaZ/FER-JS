c9063345e31229e295dcbd9c77cfe2eeec2cd856
Yevheniia Zubrych <evg.zubrich@gmail.com>
2021-04-26 00:08:14 +0300

    Refactoring, image cropping

--- public/js/videoProcessor.js
+++ public/js/videoProcessor.js
@@ -1,9 +1,9 @@
-function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
+function VideoProcessor(errorOutputId) { // eslint-disable-line no-unused-vars
     let self = this;
     this.errorOutput = document.getElementById(errorOutputId);
 
     const OPENCV_URL = 'js/opencv.js';
-    this.loadOpenCv = function (onloadCallback) {
+    this.loadOpenCv = onloadCallback => {
         let script = document.createElement('script');
         script.setAttribute('async', '');
         script.setAttribute('type', 'text/javascript');
@@ -33,11 +33,11 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
         node.parentNode.insertBefore(script, node);
     };
 
-    this.createFileFromUrl = function (path, url, callback) {
+    this.createFileFromUrl = (path, url, callback) => {
         let request = new XMLHttpRequest();
         request.open('GET', url, true);
         request.responseType = 'arraybuffer';
-        request.onload = function (ev) {
+        request.onload = ev => {
             if (request.readyState === 4) {
                 if (request.status === 200) {
                     let data = new Uint8Array(request.response);
@@ -51,12 +51,12 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
         request.send();
     };
 
-    this.loadImageToCanvas = function (url, cavansId) {
+    this.loadImageToCanvas = (url, cavansId) => {
         let canvas = document.getElementById(cavansId);
         let ctx = canvas.getContext('2d');
         let img = new Image();
         img.crossOrigin = 'anonymous';
-        img.onload = function () {
+        img.onload = () => {
             canvas.width = img.width;
             canvas.height = img.height;
             ctx.drawImage(img, 0, 0, img.width, img.height);
@@ -64,7 +64,7 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
         img.src = url;
     };
 
-    this.processVideo = function (input, output, classifier_model) {
+    this.processVideo = (input, output, classifier_model) => {
         try {
             this.clearError();
             this.run(input, output, classifier_model);
@@ -73,11 +73,11 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
         }
     };
 
-    this.clearError = function () {
+    this.clearError = () => {
         this.errorOutput.innerHTML = '';
     };
 
-    this.printError = function (err) {
+    this.printError = (err) => {
         if (typeof err === 'undefined') {
             err = '';
         } else if (typeof err === 'number') {
@@ -99,7 +99,7 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
         this.errorOutput.innerHTML = err;
     };
 
-    this.loadCode = function (scriptId, textAreaId) {
+    this.loadCode = (scriptId, textAreaId) => {
         let scriptNode = document.getElementById(scriptId);
         let textArea = document.getElementById(textAreaId);
         if (scriptNode.type !== 'text/javascript') {
@@ -108,7 +108,7 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
         textArea.value = scriptNode.text.replace(/^\n/, '');
     };
 
-    this.addFileInputHandler = function (fileInputId, canvasId) {
+    this.addFileInputHandler = (fileInputId, canvasId) => {
         let inputElement = document.getElementById(fileInputId);
         inputElement.addEventListener('change', (e) => {
             let files = e.target.files;
@@ -119,13 +119,13 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
         }, false);
     };
 
-    function onVideoCanPlay() {
+    const onVideoCanPlay = () => {
         if (self.onCameraStartedCallback) {
             self.onCameraStartedCallback(self.stream, self.video);
         }
-    }
+    };
 
-    this.startCamera = function (resolution, callback, videoId) {
+    this.startCamera = (resolution, callback, videoId) => {
         const constraints = {
             'qvga': {width: {exact: 320}, height: {exact: 240}},
             'vga': {width: {exact: 640}, height: {exact: 480}}
@@ -141,7 +141,7 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
         }
 
         navigator.mediaDevices.getUserMedia({video: videoConstraint, audio: false})
-            .then(function (stream) {
+            .then(stream => {
                 video.srcObject = stream;
                 video.play();
                 self.video = video;
@@ -149,12 +149,12 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
                 self.onCameraStartedCallback = callback;
                 video.addEventListener('canplay', onVideoCanPlay, false);
             })
-            .catch(function (err) {
+            .catch(err => {
                 self.printError('Camera Error: ' + err.name + ' ' + err.message);
             });
     };
 
-    this.stopCamera = function () {
+    this.stopCamera = () => {
         if (this.video) {
             this.video.pause();
             this.video.srcObject = null;
@@ -165,7 +165,7 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
         }
     };
 
-    this.run = function (inputId, outputId, classifier_model) {
+    this.run = (inputId, outputId, classifier_model) => {
         let video = document.getElementById(inputId);
         let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
         let dst = new cv.Mat(video.height, video.width, cv.CV_8UC4);
@@ -178,7 +178,8 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
         classifier.load(classifier_model);
 
         const FPS = 30;
-        function processVideo() {
+
+        const processVideo = () => {
             try {
                 if (!streaming) {
                     // clean and stop.
@@ -199,18 +200,24 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
                 // draw faces.
                 for (let i = 0; i < faces.size(); ++i) {
                     let face = faces.get(i);
-                    let point1 = new cv.Point(face.x, face.y);
-                    let point2 = new cv.Point(face.x + face.width, face.y + face.height);
-                    cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
+                    // let point1 = new cv.Point(face.x, face.y);
+                    // let point2 = new cv.Point(face.x + face.width, face.y + face.height);
+                    let rect = new cv.Rect(face.x, face.y, face.width, face.height);
+                    let cropped = dst.roi(rect);
+                    let dsize = new cv.Size(48, 48);
+                    cv.resize(cropped, cropped, dsize, 0, 0, cv.INTER_AREA);
+                    cv.imshow(outputId, cropped);
+                    // cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
                 }
-                cv.imshow(outputId, dst);
+                // cv.imshow(outputId, dst);
                 // schedule the next one.
-                let delay = 1000/FPS - (Date.now() - begin);
+                let delay = 1000 / FPS - (Date.now() - begin);
                 setTimeout(processVideo, delay);
             } catch (err) {
                 utils.printError(err);
             }
-        }
+        };
+
         // schedule the first one.
         setTimeout(processVideo, 0);
     };
